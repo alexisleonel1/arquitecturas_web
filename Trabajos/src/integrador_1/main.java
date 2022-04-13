a4f2caeea91bf9f72811e30c6ba89fcdfa799a5f
alexisleonel1 <alexisleogalvan12@gmail.com>
2022-04-13 17:08:05 -0300

    se agrego Apache Commons CSV

--- Trabajos/src/integrador_1/main.java
+++ Trabajos/src/integrador_1/main.java
@@ -1,15 +1,34 @@
 package integrador_1;
 
 
+import java.io.FileReader;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.SQLException;
+
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
 
 public class main {
 
 	public static void main(String[] args) {
 		Conexion conexion = new Conexion();
 		
-		crearTablas(conexion);
+		//crearTablas(conexion);
+		
+		/*CSVParser parser;
+		try {
+			parser = CSVFormat.DEFAULT.withHeader().parse(new
+					FileReader("src/integrador_1/csv/productos.csv"));
+			for(CSVRecord row: parser) {
+				System.out.println(row.get("idProducto"));
+				System.out.println(row.get("nombre"));
+				System.out.println(row.get("valor"));
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}*/
 	
 	}
 	
